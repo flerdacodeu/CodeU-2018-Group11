e74e15ecef9c49b38dd48912f41eb4483d5ca742
MernaRezk <mernarezk@hotmail.com>
2018-05-29 01:22:54 +0200

    Update Q2.java

--- mernarezk/assignment1/Q2.java
+++ mernarezk/assignment1/Q2.java
@@ -3,22 +3,27 @@ package Assignment1;
 public class Q2 {
 	/**
 	 * Method that gets Kth node from back of a singly linkedlist.
-	 * @param head of the linkedlist
-	 * @param kth node to be returned.
+	 * 
+	 * @param head
+	 *            of the linkedlist
+	 * @param kth
+	 *            node to be returned.
 	 */
-	public ListNode getKth(ListNode head, int k) {
+	public ListNode getKth(ListNode head, int k) throws IllegalStateException {
+		if (k < 0)
+			throw new IllegalStateException("K cannot be negative");
 		ListNode first = head;
 		for (int i = 0; i < k; i++) {
 			// if K > length of linkedlist.
 			if (first == null)
-				return null;
+				throw new IllegalStateException("K cannot be bigger than list size.");
 			first = first.next;
 		}
 		// Dummy node created which follows the first pointer until it hits
 		// null, then it means dummy node is currently at desired node.
 		ListNode last = new ListNode(0);
 		last.next = head;
-		
+
 		while (first != null) {
 			first = first.next;
 			last = last.next;
@@ -28,10 +33,10 @@ public class Q2 {
 }
 
 class ListNode {
-	int val;
+	Object val;
 	ListNode next;
 
-	public ListNode(int val) {
+	public ListNode(Object val) {
 		this.val = val;
 	}
 }
