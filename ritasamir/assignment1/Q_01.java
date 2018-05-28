17663bb27d30ac143e90cb2ac17f44f3c047e96b
ritasamir <ritasamir2587@gmail.com>
2018-05-28 07:29:27 +0200

    Edit checkAnagram Method

--- ritasamir/assignment1/Q_01.java
+++ ritasamir/assignment1/Q_01.java
@@ -12,26 +12,10 @@ public class Q_01 {
 	 * @return true if they are anagram, false otherwise.
 	 */
 	public boolean checkAnagram(String firstStr, String secondStr) {
-		String[] array1 = firstStr.split(" ");
-		String[] array2 = secondStr.split(" ");
-		boolean isAnagram = false;
-		if (array1.length != array2.length)
-			return false;
-		for (int i = 0; i < array1.length; i++) {
-			for (int j = 0; j < array2.length; j++) {
-				if (stringSort(array1[i]).equals(stringSort(array2[j]))) {
-					isAnagram = true;
-					break;
+	        if (stringSort(firstStr).equals(stringSort(secondStr))) {
+					return true;
 				}
-			}
-			if (isAnagram) {
-				isAnagram = false;
-			} else {
-				return false;
-			}
-
-		}
-		return true;
+		return false;
 	}
 
 	private String stringSort(String str) {
