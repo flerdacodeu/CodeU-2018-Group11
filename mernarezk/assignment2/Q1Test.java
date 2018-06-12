9dd1cd4421916230c649f818486c24c0bdea0d93
MernaRezk <mernarezk@hotmail.com>
2018-06-12 05:45:52 +0200

    Update Q1Test.java

--- mernarezk/assignment2/Q1Test.java
+++ mernarezk/assignment2/Q1Test.java
@@ -16,26 +16,14 @@ class Q1Test {
 
 	@Test
 	void testGetAncestors() {
-		tree.insert(3);
-		tree.insert(4);
-		tree.insert(1);
-		tree.insert(5);
-		tree.insert(7);
-		tree.insert(6);
-		tree.insert(2);
-		tree.insert(8);
+		int inorder[] = new int[] { 4, 2, 5, 1, 6, 3 };
+		int preorder[] = new int[] { 1, 2, 4, 5, 3, 6 };
+		tree.constructTree(preorder, inorder);
 
 		BinaryTree tree2 = new BinaryTree();
-		tree2.insert(3);
-		tree2.insert(4);
-		tree2.insert(1);
-		tree2.insert(5);
-		tree2.insert(7);
-		tree2.insert(6);
-		tree2.insert(2);
-		tree2.insert(8);
-		List<Integer> l1 = tester.getAncestors(tree.root, 7);
-		List<Integer> l2 = tester.getAncestors(tree2.root, 7);
+		tree2.constructTree(preorder, inorder);
+		List<Integer> l1 = tester.getAncestorsWithParent(tree.root, 6);
+		List<Integer> l2 = tester.getAncestorsWithParent(tree2.root, 6);
 		assertEquals(new HashSet<Integer>(l1), new HashSet<Integer>(l2));
 
 	}
