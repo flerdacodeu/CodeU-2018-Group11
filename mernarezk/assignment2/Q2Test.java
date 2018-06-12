d40001e7dad5bd3df4a879c218b3026bfd3484c1
MernaRezk <mernarezk@hotmail.com>
2018-06-12 05:45:34 +0200

    Update Q2Test.java

--- mernarezk/assignment2/Q2Test.java
+++ mernarezk/assignment2/Q2Test.java
@@ -1,31 +1,24 @@
 package Assignment2;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.HashSet;
-import java.util.List;
-
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 
-class Q1Test {
-	Q1 tester = new Q1();
-	BinaryTree tree = new BinaryTree();
+class Q2Test {
+	Q2 tester = new Q2();
+	TreeNode result;
 
 	@Test
-	void testGetAncestors() {
+	void testCommonAncestor() {
+
+		BinaryTree tree = new BinaryTree();
 		int inorder[] = new int[] { 4, 2, 5, 1, 6, 3 };
 		int preorder[] = new int[] { 1, 2, 4, 5, 3, 6 };
 		tree.constructTree(preorder, inorder);
-
-		BinaryTree tree2 = new BinaryTree();
-		tree2.constructTree(preorder, inorder);
-		List<Integer> l1 = tester.getAncestorsWithParent(tree.root, 6);
-		List<Integer> l2 = tester.getAncestorsWithParent(tree2.root, 6);
-		assertEquals(new HashSet<Integer>(l1), new HashSet<Integer>(l2));
-
+		result = tester.commonAncestor(tree.root, 4, 5);
+		assertEquals(result.val, 2);
+		 result = tester.commonAncestor(tree.root, 6, 8);
+		 assertEquals(result.val, 7);
 	}
 
 }
