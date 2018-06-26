9a5a026b0cb0ed9448aaacd96fb285bb8b19a560
MernaRezk <mernarezk@hotmail.com>
2018-06-26 22:17:34 +0200

    Update WordSearchTest.java

--- mernarezk/assignment3/WordSearchTest.java
+++ mernarezk/assignment3/WordSearchTest.java
@@ -22,15 +22,17 @@ class WordSearchTest {
 		dictionary.addWord("card");
 		dictionary.addWord("cart");
 		dictionary.addWord("cat");
-		
+		char grid2[][] = new char[][] { { 'c', 'a', 'r','d' }};
+		Dictionary dictionary3 = new Dictionary();
 		Set<String> resultContainsTwoWords = tester.searchWords(grid, dictionary);
 		Set<String> resultWithLetterUsedOnce = tester.searchWords(grid, dictionary2);
+		Set<String> resultWithTwoWordsFromDict = tester.searchWords(grid2, dictionary3);
 		assertEquals(resultWithLetterUsedOnce.contains("ccar"),false);
 		assertEquals(resultContainsTwoWords.contains("car"),true);
 		assertEquals(resultContainsTwoWords.contains("cat"),true);
 		assertEquals(resultContainsTwoWords.size(),2);
-
-
+		assertEquals(resultWithTwoWordsFromDict.contains("car"),true);
+		assertEquals(resultWithTwoWordsFromDict.contains("card"),true);
 	}
 
 }
