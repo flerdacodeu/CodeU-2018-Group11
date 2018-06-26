91fa60f182aa5fe9630ee326ac3a515ef6bbd4dc
Rita <ritasamir2587@gmail.com>
2018-06-26 18:52:20 +0200

    add more tests

--- ritasamir/assignment3/WordSearchTest.java
+++ ritasamir/assignment3/WordSearchTest.java
@@ -3,59 +3,107 @@ package assignment_3;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 import org.junit.Test;
 
 public class WordSearchTest {
 
 	Dictionary dictionary;
-	char[][] grid;
-	ArrayList<String> result;
+	HashSet<String> result;
 
 	@Test
-	public void testGetWords() {
-
-		setDictionary();
-		setGrid();
+	public void testGetWords_1() {
+		setDictionary_1();
 		WordSearch word = new WordSearch();
+		char[][] grid = { { 'a', 'a', 'r' }, { 't', 'c', 'd' } };
 		result = word.getWords(grid, dictionary);
 		assertEquals(result.size(), 3);
 		assertTrue(result.contains("car"));
 		assertTrue(result.contains("card"));
 		assertTrue(result.contains("cat"));
-
 	}
-	
+
+	@Test
+	public void testGetWords_2() {
+		setDictionary_2();
+		char[][] grid = { { 'h', 'i', 'd' }, { 'p', 'a', 'n' },
+				{ 'p', 's', 'e' }, { 's', 'k', 'l' } };
+		WordSearch word = new WordSearch();
+		result = word.getWords(grid, dictionary);
+		assertEquals(result.size(), 1);
+		assertTrue(result.contains("happiness"));
+		assertFalse(result.contains("sadness"));
+		assertFalse(result.contains("gloom"));
+		assertFalse(result.contains("boredom"));
+	}
+
 	@Test
 	public void testEmptyDictionary() {
 		dictionary = new Dictionary(new ArrayList<>());
-		setGrid();
+		char[][] grid = { { 'a', 'a', 'r' }, { 't', 'c', 'd' } };
 		WordSearch word = new WordSearch();
-		result=word.getWords(grid, dictionary);
+		result = word.getWords(grid, dictionary);
 		assertEquals(result.size(), 0);
-		
-		
 	}
 
-	private void setGrid() {
-		grid = new char[2][3];
-		grid[0][0] = 'a';
-		grid[0][1] = 'a';
-		grid[0][2] = 'r';
-		grid[1][0] = 't';
-		grid[1][1] = 'c';
-		grid[1][2] = 'd';
-
+	@Test
+	public void testEmptyGrid() {
+		setDictionary_1();
+		char[][] grid = new char[2][3];
+		WordSearch word = new WordSearch();
+		result = word.getWords(grid, dictionary);
+		assertEquals(result.size(), 0);
 	}
 
-	private void setDictionary() {
+	@Test
+	public void testGetPossiblePaths() {
+		char[][] grid = { { 'h', 'i', 'd' }, { 'p', 'a', 'n' },
+				{ 'p', 's', 'e' }, { 's', 'k', 'l' } };
+		WordSearch word = new WordSearch();
+		word.visited = new boolean[4][3];
+		ArrayList<Point> points = word.getPossiblePaths(grid, 1, 0);
+		assertEquals(points.size(), 5);
+		points = word.getPossiblePaths(grid, 1, 1);
+		assertEquals(points.size(), 8);
+		points = word.getPossiblePaths(grid, 0, 2);
+		assertEquals(points.size(), 3);
+	}
+
+	@Test
+	public void testIsWord() {
+		setDictionary_1();
+		assertTrue(dictionary.isWord("car"));
+		assertTrue(dictionary.isWord("cart"));
+		assertFalse(dictionary.isWord("crad"));
+		assertFalse(dictionary.isWord("caat"));
+	}
+
+	@Test
+	public void testIsPrefix() {
+		setDictionary_1();
+		assertTrue(dictionary.isPrefix("ca"));
+		assertTrue(dictionary.isPrefix("car"));
+		assertFalse(dictionary.isPrefix("ce"));
+		assertFalse(dictionary.isPrefix("cad"));
+	}
+
+	private void setDictionary_1() {
 		ArrayList<String> words = new ArrayList<>();
 		words.add("car");
 		words.add("card");
 		words.add("cart");
 		words.add("cat");
 		dictionary = new Dictionary(words);
+	}
 
+	private void setDictionary_2() {
+		ArrayList<String> words = new ArrayList<>();
+		words.add("happiness");
+		words.add("sadness");
+		words.add("gloom");
+		words.add("boredom");
+		dictionary = new Dictionary(words);
 	}
 
 }
