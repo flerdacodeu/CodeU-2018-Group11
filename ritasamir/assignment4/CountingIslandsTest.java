5d301530b6980274e5a9905b799dd7ebb08f6f61
Rita <ritasamir2587@gmail.com>
2018-07-10 22:37:09 +0200

    Update CountingIslandsTest.java

--- ritasamir/assignment4/CountingIslandsTest.java
+++ ritasamir/assignment4/CountingIslandsTest.java
@@ -13,24 +13,18 @@ public class CountingIslandsTest {
 		boolean[][] tiles_1 = { { false, true, false, true },
 				{ true, true, false, false }, { false, false, true, false },
 				{ false, false, true, false } };
-		assertEquals(count.getNumberOfIslands(tiles_1, 4, 4), 3);
+		assertEquals(count.getNumberOfIslands(tiles_1), 3);
 		boolean[][] tiles_2 = { { false, true, false }, { true, true, false },
 				{ false, false, true }, { false, false, true } };
-		assertEquals(count.getNumberOfIslands(tiles_2, 4, 3), 2);
+		assertEquals(count.getNumberOfIslands(tiles_2), 2);
 	}
-
+	
 	@Test
 	public void testNoIslands() {
 		boolean[][] tiles = new boolean[3][2];
-		assertEquals(count.getNumberOfIslands(tiles, 3, 2), 0);
+		assertEquals(count.getNumberOfIslands(tiles), 0);
 	}
-
-	@Test(expected = Error.class)
-	public void testWrongSize() {
-		boolean[][] tiles = new boolean[3][2];
-		count.getNumberOfIslands(tiles, -1, 3);
-	}
-
+	
 	@Test
 	public void testGetPossibleNeighbours() {
 		boolean[][] tiles = { { false, true, false, true },
@@ -40,5 +34,4 @@ public class CountingIslandsTest {
 		assertEquals(count.getPossibleNeighbours(tiles, 0, 3).size(), 0);
 		assertEquals(count.getPossibleNeighbours(tiles, 1, 1).size(), 2);
 	}
-
 }
