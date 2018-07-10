e48f1bd65da641de25da61516a8e712595ace372
Rita <ritasamir2587@gmail.com>
2018-07-10 22:36:04 +0200

    Update CountingIslands.java

--- ritasamir/assignment4/CountingIslands.java
+++ ritasamir/assignment4/CountingIslands.java
@@ -6,8 +6,6 @@ import java.util.Iterator;
 import assignment_3.Point;
 
 public class CountingIslands {
-	int rowNumber;
-	int columnNumber;
 
 	/**
 	 * @param tiles
@@ -19,24 +17,22 @@ public class CountingIslands {
 	 *            is the number of columns
 	 * @return the number of islands.
 	 */
-	public int getNumberOfIslands(boolean[][] tiles, int rowNumber,
-			int columnNumber) {
-		if (tiles.length != rowNumber || tiles[0].length != columnNumber)
-			throw new Error("the size doesn't match");
+	// looping through the 2d array and search for an island when we find a land
+	// w do a dfs search to get all possible lands next to that land, while
+	// doing the dfs we convert the land to water so we won't count this island
+	// again and finally increasing the counter of islands by one every time we
+	// start with a land.
+	public int getNumberOfIslands(boolean[][] tiles) {
 		int counter = 0;
-		this.rowNumber = rowNumber;
-		this.columnNumber = columnNumber;
-		for (int i = 0; i < rowNumber; i++) {
-			for (int j = 0; j < columnNumber; j++) {
+		for (int i = 0; i < tiles.length; i++) {
+			for (int j = 0; j < tiles[0].length; j++) {
 				if (tiles[i][j]) {
 					counter++;
 					dfs(i, j, tiles);
 				}
 			}
 		}
-
 		return counter;
-
 	}
 
 	private void dfs(int row, int col, boolean[][] tiles) {
@@ -46,18 +42,17 @@ public class CountingIslands {
 			Point point = itr.next();
 			dfs(point.x, point.y, tiles);
 		}
-
 	}
 
 	ArrayList<Point> getPossibleNeighbours(boolean[][] tiles, int row, int col) {
 		ArrayList<Point> points = new ArrayList<>();
-		if (row + 1 < rowNumber && tiles[row + 1][col]) {
+		if (row + 1 < tiles.length && tiles[row + 1][col]) {
 			points.add(new Point(row + 1, col));
 		}
 		if (row - 1 > -1 && tiles[row - 1][col]) {
 			points.add(new Point(row - 1, col));
 		}
-		if (col + 1 < columnNumber && tiles[row][col + 1]) {
+		if (col + 1 < tiles[0].length && tiles[row][col + 1]) {
 			points.add(new Point(row, col + 1));
 		}
 		if (col - 1 > -1 && tiles[row][col - 1]) {
@@ -65,5 +60,4 @@ public class CountingIslands {
 		}
 		return points;
 	}
-
 }
